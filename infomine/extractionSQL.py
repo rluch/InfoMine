d8ca96053975be534fe99559bb5d51bd006349f1
henrikholm89 <henrikholm89@gmail.com>
2014-11-09 00:31:17 +0100

    Så er der en json fil, men måske den skal lave anderledes.

--- infomine/extractionSQL.py
+++ infomine/extractionSQL.py
@@ -1,17 +1,27 @@
 __author__ = 'Henrik'
 
 import mysql.connector
-import codecs
 import csv
+import json
+import os
+import pprint
 
-comment = []
 firstName = []
-gender = []
 drengenavne = []
+pigenavne = []
+unisexnavne = []
 
 with open("/Users/Henrik/InfoMine/data/drengenavne.csv", "r") as in_file:
     for line in csv.reader(in_file):
          drengenavne.append(line[0].lower())
+
+with open("/Users/Henrik/InfoMine/data/pigenavne.csv", "r") as in_file:
+    for line in csv.reader(in_file):
+         pigenavne.append(line[0].lower())
+
+with open("/Users/Henrik/InfoMine/data/unisexnavne.csv", "r") as in_file:
+    for line in csv.reader(in_file):
+         unisexnavne.append(line[0].lower())
 
 
 cnx = mysql.connector.connect(user='root', password='',
@@ -19,42 +29,59 @@ cnx = mysql.connector.connect(user='root', password='',
                               database='Information')
 
 cursor = cnx.cursor()
-cursor1 = cnx.cursor()
-
-query = ("SELECT u.u_name, c.c_body FROM Information.inf_dtu_user u, Information.inf_dtu_comment c where u.u_uid=c.c_uid")
-
+query = ("SELECT u_name FROM Information.inf_dtu_user")
 cursor.execute(query)
 
 for c in cursor:
     if c[0] != None:
         name = c[0]
-        #firstName = name.split()[0].lower()
         firstName.append(name.split()[0].lower())
-        #query2 = ("UPDATE Information.inf_dtu_user SET u_firstName=firstName")
-        #cursor1.execute(query2)
 
-        #for dn in drengenavne:
-        #    if firstName == dn:
+gender_name_list = []
+firstName = set(firstName)
+for fn in firstName:
+    for dn in drengenavne:
+        if dn == fn:
+            gender_name_list.append((fn,'Male'))
+    for pn in pigenavne:
+        if pn == fn:
+            gender_name_list.append((fn,'Female'))
+    #for un in unisexnavne:
+    #    if un == fn:
+    #        gender_name_list.append((fn,'Unisex'))
+
+c_data_set = []
+gender_data_set = []
+
+query = ("SELECT u.u_name, c.c_body FROM Information.inf_dtu_user u, Information.inf_dtu_comment c where u.u_uid=c.c_uid")
+cursor.execute(query)
+
+for c in cursor:
+    if c[0] != None:
+        name = c[0]
+        comment = c[1]
+        firstName = name.split()[0].lower()
+        for gnl in gender_name_list:
+            if gnl[0] == firstName:
+                c_data_set.append(comment)
+                gender_data_set.append(gnl[1])
+
 
 cursor.close()
 cnx.close()
 
-#with codecs.open("/Users/Henrik/InfoMine/data/drengenavne.csv", "r", "utf-8") as in_file:
-#    for line in in_file.readlines():
-#         drengenavne.append(line)
+gender_and_comments_file = "gender_and_comments.json"
 
-print drengenavne[1]
-print len(drengenavne[1])
-print firstName[1]
-print len(firstName[1])
-print type(drengenavne[1])
-print type(firstName[1])
+if not os.path.isfile(gender_and_comments_file):
+        with open(gender_and_comments_file, "w") as out_file:
+            out_file.write("")
 
-#for dn in drengenavne:
-#    for fn in firstName:
-#        if dn == fn:
+with open(gender_and_comments_file, "w") as outfile:
+    json.dump({'gender':gender_data_set, 'comment':c_data_set, }, outfile, indent=2)
+
+with open(gender_and_comments_file) as data_file:
+    data = json.load(data_file)
 
 
 
 
-#print gender[1]
