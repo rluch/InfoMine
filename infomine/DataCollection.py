4e7c4411ad989d11cb41c9cfb476566a6ef9e834
henrikholm89 <henrikholm89@gmail.com>
2014-11-15 13:17:58 +0100

    lavet ændringer som helge kom med
    brugt intersection, lavet filer som input og slette __init__, da den
    ikke giver mening imo.

--- infomine/DataCollection.py
+++ infomine/DataCollection.py
@@ -4,8 +4,10 @@ import mysql.connector
 import csv
 import os
 import itertools
+import codecs
 
-class DataCollection():
+
+class DataCollection:
 
     def read_in_danish_names_from_files(self):
 
@@ -21,13 +23,13 @@ class DataCollection():
             for line in csv.reader(in_file):
                 pigenavne.append(line[0].lower())
 
-        #with open("/Users/Henrik/InfoMine/data/unisexnavne.csv", "r") as in_file:
-        #    for line in csv.reader(in_file):
-        #        unisexnavne.append(line[0].lower())
+        with open("data/unisexnavne.csv", "r") as in_file:
+            for line in csv.reader(in_file):
+                unisexnavne.append(line[0].lower())
 
-        return drengenavne, pigenavne
+        return drengenavne, pigenavne, unisexnavne
 
-    def load_names_from_sql_dump(self):
+    def load_firstnames_from_sql_dump(self):
 
         firstname = []
 
@@ -49,20 +51,27 @@ class DataCollection():
 
         return firstname
 
-    def find_gender_by_name(self, firstname, drengenavne, pigenavne):
+    def find_gender_by_name(self, firstname, drengenavne, pigenavne, unisexnavne):
 
         gender_name_list = []
-        firstname = set(firstname)
-        for fn in firstname:
-            for dn in drengenavne:
-                if dn == fn:
-                    gender_name_list.append((fn,'Male'))
-            for pn in pigenavne:
-                if pn == fn:
-                    gender_name_list.append((fn,'Female'))
-            #for un in unisexnavne:
-                #if un == fn:
-#                   #gender_name_list.append((fn,'Unisex'))
+
+        # Find the male names that are also unisex names
+        unisex_in_male = set(drengenavne).intersection(unisexnavne)
+
+        # Find all male names that is used in the comments and labels it minus unisex names
+        male_name_list = set(drengenavne).intersection(set(firstname)) - unisex_in_male
+
+        for mnl in male_name_list:
+            gender_name_list.append((mnl,'Male'))
+
+        # Find the female names that are also unisex names
+        unisex_in_female = set(pigenavne).intersection(unisexnavne)
+
+        # Find all female names that is used in the comments and labels it minus unisex names
+        female_name_list = set(pigenavne).intersection(set(firstname)) - unisex_in_female
+
+        for fnl in female_name_list:
+            gender_name_list.append((fnl,'Female'))
 
         return gender_name_list
 
@@ -94,43 +103,46 @@ class DataCollection():
 
         return gender_data_set, c_data_set
 
-    def save_gender_with_comments_to_file(self, gender_data_set, c_data_set):
+    def save_gender_with_comments_to_file(self, gender_data_set, c_data_set, filename):
 
-        gender_and_comments_file = "gender_and_comments.csv"
+        training_set_file = filename+'.csv'
 
-        if not os.path.isfile(gender_and_comments_file):
-                 with open(gender_and_comments_file, "w") as out_file:
+        if not os.path.isfile(training_set_file):
+                 with open(training_set_file, "w") as out_file:
                      out_file.write("")
 
-        with open(gender_and_comments_file, 'wb') as f:
+        with open(training_set_file, 'wb') as f:
             writer = csv.writer(f)
             writer.writerows(itertools.izip(gender_data_set, c_data_set))
 
-    def load_gender_with_comments_from_file(self):
+    def load_gender_with_comments_from_file(self, filename):
 
-        gender_and_comments_file = "gender_and_comments.csv"
+        training_set_file = filename+'.csv'
 
         trainingSet = []
 
-        with open(gender_and_comments_file, "r") as in_file:
+        with open(training_set_file, "r") as in_file:
             for line in csv.reader(in_file):
-                trainingSet.append((line[0],line[1]))
+                trainingSet.append((line[0], line[1]))
 
         return trainingSet
 
-    def __init__(self):
-        firstname = self.load_names_from_sql_dump()
-        drengenavne, pigenavne = self.read_in_danish_names_from_files()
+    #def __init__(self, training_set):
+    #    trainingSet = self.load_gender_with_comments_from_file("testing")
+    #    self.training_set = trainingSet
 
 dc = DataCollection()
-drengenavne, pigenavne = dc.read_in_danish_names_from_files()
-firstname = dc.load_names_from_sql_dump()
-gender_name_list = dc.find_gender_by_name(firstname, drengenavne, pigenavne)
+drengenavne, pigenavne, unisexnavne = dc.read_in_danish_names_from_files()
+firstname = dc.load_firstnames_from_sql_dump()
+gender_name_list = dc.find_gender_by_name(firstname, drengenavne, pigenavne, unisexnavne)
 gender_data_set, c_data_set = dc.load_comments_from_database_and_combine_with_gender(gender_name_list)
-dc.save_gender_with_comments_to_file(gender_data_set,c_data_set)
-trainingSet = dc.load_gender_with_comments_from_file()
+dc.save_gender_with_comments_to_file(gender_data_set,c_data_set,"testing")
+trainingSet = dc.load_gender_with_comments_from_file("testing")
 
 print trainingSet[0]
+
+#dc1 = DataCollection().load_gender_with_comments_from_file("testing")
+#print dc1[0]
 
 # json
 # #with open(gender_and_comments_file, "w") as outfile:
