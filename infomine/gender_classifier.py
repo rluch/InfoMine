834b163682f48a8fa75d14a04c7d182909d0a646
henrikholm89 <henrikholm89@gmail.com>
2014-11-17 16:24:56 +0100

    sentiment implementeret

--- infomine/gender_classifier.py
+++ infomine/gender_classifier.py
@@ -6,65 +6,86 @@ import nltk
 from collections import Counter
 import DataCollection as dc
 
-class gender_classifier():
-    def tokenize_line(self, comment):
-        return nltk.word_tokenize(comment)
+# class gender_classifier():
+#     def tokenize_line(self, comment):
+#         return nltk.word_tokenize(comment)
 
-    def sentiment_danish_words(self):
-        word = []
-        sentScore = []
+def sentiment_danish_words():
+    word = []
+    sentScore = []
 
-        # Pair each wood with its average sentiment
-        with open("/Users/Henrik/InfoMine/data/Nielsen2011Sentiment_afinndk-2.txt", 'r') as in_file:
-            for line in in_file.readlines()[0:]:
-                word.append(line.split('\t')[0]) # Each column in the file is tab seperated
-                tab_split= line.split('\t')[1]
-                newline_split = tab_split.split('\n')[0]
-                sentScore.append(newline_split)
+    # Pair each wood with its average sentiment
+    with open("/Users/Henrik/InfoMine/data/Nielsen2011Sentiment_afinndk-2.txt", 'r') as in_file:
+        for line in in_file.readlines()[0:]:
+            word.append(line.split('\t')[0]) # Each column in the file is tab seperated
+            tab_split = line.split('\t')[1]
+            newline_split = tab_split.split('\n')[0]
+            sentScore.append(newline_split)
 
-        sentiment = dict(zip(word, sentScore))
+    sentiment = dict(zip(word, sentScore))
 
-        return sentiment
+    return sentiment
 
-    def preprocessing(self, comment):
-        features = {}
-        words = nltk.word_tokenize(comment.decode("utf-8"))
-        sentences = nltk.sent_tokenize(comment.decode("utf-8"))
-        features["number_of_words"] = len(words)
-        features["number_of_sentences"] = len(sentences)
+def preprocessing(comment, sentiment):
+    features = {}
+    words = nltk.word_tokenize(comment.decode("utf-8"))
+    sentences = nltk.sent_tokenize(comment.decode("utf-8"))
+    features["number_of_words"] = len(words)
+    features["number_of_sentences"] = len(sentences)
 
-        return features
+    lower_words = []
+    count = 0
+    sentValue = 0
+    
+    for word in words:
+        lower_words.append(word.lower())
 
-    def naive_bayes_classification(self, features):
-        splitdata = len(features)/2
+    #print list(filter((lambda key: key in lower_words), sentiment))
+    # check if word appears in the dictionary created ealier.
 
-        featuresets = [(self.preprocessing(comment), gender) for (comment, gender) in features]
-        train_set, test_set = featuresets[:splitdata], featuresets[splitdata:]
-        classifier = nltk.NaiveBayesClassifier.train(train_set)
+    for key in sentiment:
+        if key in lower_words:
+            count += 1
+            sentValue += float(sentiment[key])
+    if count > 0:
+        avg_sent_of_comment = sentValue/count
+    else:
+        avg_sent_of_comment = 0
+    features["average_sentiment"] = avg_sent_of_comment
+    return features
 
-        testError = nltk.classify.accuracy(classifier, test_set)
-        trainError = nltk.classify.accuracy(classifier, train_set)
-        classifier.show_most_informative_features(3)
-        print testError
-        print trainError
 
-        return featuresets
+def naive_bayes_classification(features, sentiment):
 
-     #def feature_extractor(self, comment):
-        # features = {}
-        # words = nltk.word_tokenize(comment[0])
-        # sentences = nltk.sent_tokenize(comment[0])
-        # print words
-        # features["number_of_words"] = len(words)
-        # features["number_of_sentences"] = len(sentences)
-        # #tokenized = self.tokenize_line(comment.translate(None, string.punctuation))
-        # #tokenized = self.tokenize_line(comment)
-        # #wordpunct_tokenize(raw)
-        # #for word in tokenized:
-        # #    if word not in nltk.corpus.stopwords.words('danish') and word not in '.,-@#"\'':
-        # #        if word not in features:
-        # #            features.append(word)
-        # return features
+    splitdata = len(features)/2
+
+    featuresets = [(preprocessing(comment, sentiment), gender) for (comment, gender) in features]
+    train_set, test_set = featuresets[:splitdata], featuresets[splitdata:]
+    classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+    test_accuracy = nltk.classify.accuracy(classifier, test_set)
+    train_accuracy = nltk.classify.accuracy(classifier, train_set)
+    classifier.show_most_informative_features(5)
+    print test_accuracy
+    print train_accuracy
+
+    return featuresets
+
+ #def feature_extractor(self, comment):
+    # features = {}
+    # words = nltk.word_tokenize(comment[0])
+    # sentences = nltk.sent_tokenize(comment[0])
+    # print words
+    # features["number_of_words"] = len(words)
+    # features["number_of_sentences"] = len(sentences)
+    # #tokenized = self.tokenize_line(comment.translate(None, string.punctuation))
+    # #tokenized = self.tokenize_line(comment)
+    # #wordpunct_tokenize(raw)
+    # #for word in tokenized:
+    # #    if word not in nltk.corpus.stopwords.words('danish') and word not in '.,-@#"\'':
+    # #        if word not in features:
+    # #            features.append(word)
+    # return features
 
 
     #def __init__(self, comment):
@@ -73,9 +94,11 @@ class gender_classifier():
         #print featuresets
 
 training = dc.load_gender_with_comments_from_file("gender_and_comments")
-#testing = gender_classifier().preprocessing(training[50][0])
-featureSet = gender_classifier().naive_bayes_classification(training)
-#sentiment = gender_classifier().sentiment_danish_words()
+sentiment_danish = sentiment_danish_words()
+#testing = preprocessing(training[50][0], sentiment_danish)
+#print testing
+featureSet = naive_bayes_classification(training[0:20], sentiment_danish)
+
 #print sentiment
 
 
